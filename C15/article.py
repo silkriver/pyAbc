8f573df9b84a883909c2266826ef31afd6e0e6de
freesand <yuqinju@163.com>
2020-03-30 10:38:42 +0800

    C15.2.3 分页功能的实现

--- C15/article.py
+++ C15/article.py
@@ -1,10 +1,15 @@
 """在线文章聚合展示"""
 from flask import Flask, render_template
 from datetime import datetime
+from flask_paginate import Pagination, get_page_args
 import sqlite3
 import time
 import json
 app = Flask(__name__)
+
+
+def get_rows(rows, offset=0, per_page=10):
+    return rows[offset:offset + per_page]
 
 
 @app.route("/")
@@ -21,4 +26,12 @@ def index():
     for r in result:
         rows.append(json.loads(r[0]))
     rows.sort(key=lambda i: i["date"], reverse=True)
-    return render_template("index.html", rows=rows)
+    page, per_page, offset = get_page_args(
+        page_parameter="page", per_page_parameter="per_page")
+    pagination_rows = get_rows(rows=rows, offset=offset, per_page=per_page)
+    pagination = Pagination(
+        page=page, per_page=per_page, total=len(rows),
+        css_framework="bootstrap4")
+    return render_template(
+        "index.html", rows=pagination_rows,
+        page=page, per_page=per_page, pagination=pagination)
